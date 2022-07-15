04b7868f5c405bdd79db7d3b7f9d47dde9c1e275
chiyinwong <chiyinwong97@gmail.com>
2022-07-15 16:37:20 +1000

    Finished lesson 4

--- data-analysis-panda-numpys-seaborn/Part-7-reading-data/unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
+++ data-analysis-panda-numpys-seaborn/Part-7-reading-data/unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
@@ -3,5 +3,21 @@ import pandas as pd
 
 col_names = ['Title', 'Air date', 'Production code', 'Season', 'Number in season',
              'Number in series', 'US viewers (million)', 'Views', 'IMDB rating']
+
+filepath = 'files/simpsons-episodes.tsv'
+
+# use sep='\t'
+
+simpsons = pd.read_csv(sep='\t',
+                       # INCORRECT: for tsv files, need to include parameter encoding='UTF-8',
+                       names=col_names,
+                       usecols=['Title', 'Air date', 'Production code', 'IMDB rating'], 
+                       skiprows=4,
+                       index_col=[2], # or just use index_col='Production code'
+                       na_values='no_val',
+                       parse_dates=[1]) # or just use parse_dates='Air date'
+
+
+
              
                           
