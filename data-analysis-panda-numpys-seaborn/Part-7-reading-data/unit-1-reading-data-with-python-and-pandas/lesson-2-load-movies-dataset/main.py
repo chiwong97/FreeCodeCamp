daee6b0287a8c9ed5be5fe41059867c602de7b83
chiyinwong <chiyinwong97@gmail.com>
2022-07-15 14:12:42 +1000

    Finished lesson 2

--- data-analysis-panda-numpys-seaborn/Part-7-reading-data/unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
+++ data-analysis-panda-numpys-seaborn/Part-7-reading-data/unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
@@ -5,3 +5,6 @@ column_names = ['color', 'director_name', 'num_critic_for_reviews', 'duration',
                 'gross', 'movie_title', 'num_user_for_reviews',	'country',
                 'cotent_rating', 'budget', 'title_year', 'imdb_score', 'genre']
 
+filepath = 'files/movies.csv'
+
+movies = pd.read_csv(filepath, header=None, names=column_names, sep='|', na_values='?', skiprows=3)
